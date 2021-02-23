6392431f64c7df732e0b551bd8ecf1394a98481a
awesomejim <jimnahgua@gmail.com>
2021-02-23 22:10:46 +0300

    - Added Saved Recipe view controller

---
+++ Easy Super Meals/Controller/SavedRecipeDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SavedRecipeDetailsViewController.swift
+//  Easy Super Meals
+//
+//  Created by James Mbugua on 23/02/2021.
+//
+
+import UIKit
+
+class SavedRecipeDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
